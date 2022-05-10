4ba9803353e924cebe1078a1e69f7b7732b6120b
servo pcerda <fernandonando98@live.com.mx>
2022-05-09 20:02:35 -0500

    feat: first select with real database

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # db_with_node
+
 Conexión a base de datos con javascript
+
+## How to run this code
+
+- `npm i`
+- Clone `.env.example` into `.env` file with the credentials
--- app.js
+++ app.js
@@ -2,21 +2,19 @@ import knex from 'knex';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(
-  process.env.DB_HOST,
-  process.env.DB_PORT,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  process.env.DB_NAME
-);
+const connection = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+});
 
-// knex({
-//   client: 'pg',
-//   connection: {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//   },
-// });
+const query = connection('film').where('release_year', 2006);
+
+query.then((rows) => {
+  console.log(rows);
+});
